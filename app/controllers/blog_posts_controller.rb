a09e304b7968e9f8681267afe4a8fcc394245cf3
joao cortat <jmcortat@gmail.com>
2023-09-20 14:37:51 +0200

    Refactoring routes and before_action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,10 +1,11 @@
 class BlogPostsController < ApplicationController
+  before_action :set_blog_posts, only: %i[show edit update destroy]
+
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_post = BlogPost.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path, flash: { error: 'Blog post not found' }
   end
@@ -23,12 +24,9 @@ class BlogPostsController < ApplicationController
     end
   end
 
-  def edit
-    @blog_post = BlogPost.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @blog_post = BlogPost.find(params[:id])
     if @blog_post.update(blog_post_params)
       redirect_to blog_post_path(@blog_post)
     else
@@ -37,12 +35,17 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
     @blog_post.destroy
     redirect_to root_path
   end
 
   private
+
+  def set_blog_posts
+    @blog_post = BlogPost.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
+  end
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
